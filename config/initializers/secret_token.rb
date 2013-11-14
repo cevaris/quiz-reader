d7ce3818e3d6900c7dcfc555211d957a44c4d13d
cevaris <cevaris@gmail.com>
2013-11-14 14:10:37 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuizReader::Application.config.secret_key_base = '727beadb18780ac6bd96a1bf5ca0a5e20e6608e77c97bf2a9644eab562b8a05e7554bd87d534ffa3a507df322e426edd62c7973580fa273fe2496d2de86fccf5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuizReader::Application.config.session_store :cookie_store, key: '_quiz-reader_session'
